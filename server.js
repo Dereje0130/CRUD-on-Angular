865d9754c32b03ccb3511a022f8603d4c0013769
Dereje <derejenet12@gmail.com>
2022-03-23 00:07:38 -0400

    final commit

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/new-crud-project'));
+app.use(express.static(__dirname + '/dist/angular-crud-project'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/new-crud-project/index.html'));});
+'/dist/angular-crud-project/index.html'));});
 app.listen(process.env.PORT || 8080);

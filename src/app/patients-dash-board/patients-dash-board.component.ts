269286628474749d7bee4e83dbd9fe9900b348d0
Dereje <derejenet12@gmail.com>
2022-03-24 19:37:51 -0400

    corrected

--- src/app/patients-dash-board/patients-dash-board.component.ts
+++ src/app/patients-dash-board/patients-dash-board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder,FormGroup} from '@angular/forms'
+import { sortAndDeduplicateDiagnostics } from 'typescript';
 import { ApiService } from '../Shared/api.service';
 import { patientsModel } from './patients-dashboard.model';
 
@@ -32,6 +33,14 @@ export class PatientsDashBoardComponent implements  OnInit{
       this.patientModelObj.medicalrecordNumber = this.formValue.value.medicalrecordNumber;
       this.patientModelObj.phoneNumber = this.formValue.value.phoneNumber;
       this.patientModelObj.emailAddress = this.formValue.value.emailAddress;
+      
+    this.Api.postPatients(this.patientModelObj).subscribe(res=>{
+      console.log(res);
+      alert("Patient Added Successfully")
+    },
+    err=>{
+      alert("Something went wrong")
+    }) 
 
     }
   }
